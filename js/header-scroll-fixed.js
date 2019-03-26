3e529ab308bf38a79490f2347a4573e1d9abf58b
Rafael Boiko <rafaelboiko@me.com>
2019-03-26 14:11:23 -0300

    Add files via upload

--- js/header-scroll-fixed.js
+++ js/header-scroll-fixed.js
@@ -1,7 +1,7 @@
 $(window).scroll(function() {    
     var scroll = $(window).scrollTop();
 
-    if (scroll >= 557) {
+    if (scroll >= 565) {
         $(".fix-row").addClass("fixed");
     } else {
         $(".fix-row").removeClass("fixed");
