c2f9555c4619f93f4429a44f22ccf3bafd26d0de
Rafael Boiko <rafaelboiko@icloud.com>
2019-03-27 14:47:25 -0300

    update 3

--- js/header-scroll-fixed.js
+++ js/header-scroll-fixed.js
@@ -1,7 +1,7 @@
 $(window).scroll(function() {    
     var scroll = $(window).scrollTop();
 
-    if (scroll >= 855) {
+    if (scroll >= 642) {
         $(".fix-row").addClass("fixed");
     } else {
         $(".fix-row").removeClass("fixed");
